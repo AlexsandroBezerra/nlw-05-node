7553780ae980c6af4c46c69c422a0fa59a23b6ae
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-04-21 08:05:08 -0300

    🚀 Second day

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+''
+import { SettingsRepository } from '../repositories/SettingsRepository'
+
+export class SettingsController {
+  async create(request: Request, response: Response) {
+    const { username } = request.body
+
+    const settingsRepository = getCustomRepository(SettingsRepository)
+
+    const setting = settingsRepository.create({
+      username
+    })
+
+    await settingsRepository.save(setting)
+
+    return response.json(setting)
+  }
+}
